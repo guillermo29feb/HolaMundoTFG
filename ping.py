231ae86d3f5b9a62f516d6f8195468cd4265519a
guillermo29feb <izquierdo_69_3@hotmail.com>
2016-03-16 23:21:29 +0100

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,8 +1,10 @@
 from scapy.all import IP,ICMP,send
 
-#ip destino del ping
+# ip destino del ping
 ip = IP(dst="www.esiiab.uclm.es")
 icmp = ICMP()
+
+# creacion del paquete
 ping = ip/icmp
 
 ping.show()
